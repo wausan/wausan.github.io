de344515220e3358f870b6682ea0264c43b074fa
bossmaw <wausan_230000001410@uic.edu.ph>
2024-10-21 09:20:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+const checkbox = document.getElementById("darkModeCheckbox");
+const body = document.body;
+
+// Event listener to toggle dark mode based on checkbox
+checkbox.addEventListener("change", () => {
+    body.classList.toggle("dark-mode", checkbox.checked);
+});
