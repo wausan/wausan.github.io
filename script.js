50c56344075f9db3996a067eefc107e5ca8b16ec
bossmaw <wausan_230000001410@uic.edu.ph>
2024-11-22 06:22:36 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const checkbox = document.getElementById("darkModeCheckbox");
-const body = document.body;
-
-// Event listener to toggle dark mode based on checkbox
-checkbox.addEventListener("change", () => {
-    body.classList.toggle("dark-mode", checkbox.checked);
-});
